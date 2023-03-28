17570c5acca8eefc7b665e346ceb69bd1d52becc
Ian Lee <irl5041@psu.edu>
2023-03-28 13:07:20 -0400

    Add files via upload

---
+++ Preprocessing/IRIS_DL.py
@@ -0,0 +1,51 @@
+# Script to download seismic data from a specified time window
+
+# Import modules
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Change these only
+# --------------------------------------------------------------------------
+user, pw = "penguin@psu.edu", "1234567890"  # Username and password
+source = "service.iris.edu/ph5ws/dataselect/1/queryauth?"  # Download source
+time_string = (
+    "starttime=2023-01-08T12%3A00%3A00.000000&"
+    + "endtime=2023-01-08T12%3A01%3A00.000000"
+)  # Start and end times, "%3A" is url encoding for ":"
+# Download specifications
+network, station, loc, channel, fmt = (
+    "Y2",
+    "71%2A",  # "%2A" is url encoding for "*" and represents all stations starting with "71"
+    "--",
+    "GP%2A",  # "%2A" is url encoding for "*" and represents all stations starting with "GP"
+    "mseed",  # Other options are "segy1", "segy2", and "sac"
+)
+# --------------------------------------------------------------------------
+
+# Generate download URL
+url_path = (
+    "http://"
+    + user
+    + ":"
+    + pw
+    + "@"
+    + source
+    + time_string
+    + "&network="
+    + network
+    + "&station="
+    + station
+    + "&location="
+    + loc
+    + "&channel="
+    + channel
+    + "&format="
+    + fmt
+)
+
+# Download data
+ser = Service(ChromeDriverManager().install())
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)  # Use the chrome browser
+driver.get(url_path)  # Access URL and download data
