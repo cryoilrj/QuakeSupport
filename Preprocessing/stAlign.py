0c8366fc06381903a24a4f3b0551c806d05452dc
Ian Lee <irl5041@psu.edu>
2023-04-12 12:32:50 -0400

    Commit for V2
    Expanded modifiable variables for more control, enabled re-alignment for variable start times (including a mix of later and earlier start times), added conditions to handle errors, and increased verbosity of output.

--- Preprocessing/stAlign.py
+++ Preprocessing/stAlign.py
@@ -1,29 +1,44 @@
 # Script to align stream trace times
 
 # Import modules
-import glob
+import sys
+from glob import glob
 from obspy import read
+from obspy import UTCDateTime
 
 # Change these only
-strms = sorted(glob.glob("/path/to/folder/*.mseed*"))  # Sorted list of mSEED files
-fs = 1000  # Trace sampling frequency
+# -------------------------------------------------------------------------------------
+strms = sorted(glob("/path/to/folder/*.mseed*"))  # List of mSEED files
+fs = 1000  # Trace sampling frequency, in Hz
+chunk = 7200  # Chunk size of streams, in seconds
+# List of original start times in UTCDateTime
+starttimes = [UTCDateTime("2023-01-30T23:55:00.000000") + c * chunk for c in range(12)]
+# -------------------------------------------------------------------------------------
 
 # Loop through mSEED files
-for s in strms:
-    strm = read(s)
-    starttimes = [t.stats.starttime for t in strm]  # Start times of all stream traces
+if len(strms) != len(starttimes):
+    print("Error: listed number of mSEED files and original start times do not match")
+    sys.exit(1)
 
-    # Apply alignment
-    shift = max(starttimes) - min(starttimes)  # Compute the time shift
-    if shift < (1 / fs):  # Less than sampling period
-        for tr in strm:
-            if tr.stats.starttime < max(starttimes):
-                tr.stats.starttime += shift  # Align streams nominally
+# Align traces to original start time
+for s in range(len(strms)):
+    strm = read(strms[s])  # Read mSEED file
+    starttime = starttimes[s]  # Original start time
+    shifts = []  # List of time shifts
+    for tr in strm:
+        shift = starttime - tr.stats.starttime  # Required time shift
+        if shift < (1 / fs):  # Check if time shift < sampling period
+            tr.stats.starttime += shift  # Apply time shift
+            shifts.append(shift)  # Record time shift
+        else:
+            print("Error: time shift is greater than or equal to sampling period")
+            print(tr)
+            sys.exit(1)
 
     # Write aligned mSEED files
-    print(shift, "shift")
+    print("Indexed time shifts:", [(i, ts) for i, ts in enumerate(shifts)])
     print(strm.__str__(extended=True), "\n")
-    splitstr = s.split("/")
-    strm.write("/".join(splitstr[:-1]) + "/aligned_" + splitstr[-1], format="MSEED")
+    splitstrm = strms[s].split("/")
+    strm.write("/".join(splitstrm[:-1]) + "/aligned_" + splitstrm[-1], format="MSEED")
 
-print("Streams aligned")
+print("Stream trace times aligned")
